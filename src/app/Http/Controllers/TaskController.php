cf87b5d7b905e612b5d2bae598bd3ad93b100a29
budou114 <58349030+budou114@users.noreply.github.com>
2024-07-07 15:22:21 +0900

    first commit

---
+++ src/app/Http/Controllers/TaskController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::all();
+    }
+
+    public function store(Request $request)
+    {
+        $task = Task::create($request->all());
+        return response()->json($task, 201);
+    }
+
+    public function show($id)
+    {
+        return Task::find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+        return response()->json($task, 200);
+    }
+
+    public function destroy($id)
+    {
+        Task::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
+}
